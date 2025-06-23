d2fb95c1bf28353cf1a8044869025b0a8fab82fe
mokgavdilabs <mok@gavdilabs.com>
2025-06-23 11:09:42 +0200

    UI updates

---
+++ app/myapp/webapp/controller/Cart.controller.ts
@@ -0,0 +1,13 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace com.myorg.myapp.controller
+ */
+export default class Cart extends BaseController {
+
+	
+	public onInit(): void {
+		
+	}
+
+}
--- app/myapp/webapp/controller/Main.controller.ts
+++ app/myapp/webapp/controller/Main.controller.ts
@@ -8,6 +8,20 @@ import Filter from "sap/ui/model/Filter";
 import FilterOperator from "sap/ui/model/FilterOperator";
 import GridList from "sap/f/GridList";
 import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
+import ODataModel from "sap/ui/model/odata/v4/ODataModel";
+import JSONModel from "sap/ui/model/json/JSONModel";
+
+enum StockStatus {
+	InStock = "Available",
+	OutOfStock = "Out of Stock !!!"
+}
+
+type cartObject = {
+	productId: string,
+	productTitle: string,
+	quantity: int,
+	price: float
+}
 
 /**
  * @namespace com.myorg.myapp.controller
@@ -17,6 +31,41 @@ export default class Main extends BaseController {
 	private oDialog: Popover;
 	public onInit(): void {
 		
+	}
+
+	public async onPressProduct(oEvent: Button$PressEvent) {
+		const oContext = oEvent.getSource().getBindingContext();
+		const sStoreId = oContext.getProperty("store_storeId") as string;
+		const sProductId = oContext.getProperty("productId") as string;
+		const sProductTitle = oContext.getProperty("title") as string;
+		const fPrice = oContext.getProperty("price") as float;
+
+		const oModel = this.getView().getModel() as ODataModel;
+		const oContextBinding = oModel.bindContext("/checkStockInStore(...)");
+		oContextBinding.setParameter("storeId", sStoreId);
+		oContextBinding.setParameter("productId", sProductId);
+		oContextBinding.setParameter("appliedQty", 1);
+
+	    await oContextBinding.invoke().then( () => {
+			const sInStock = oContextBinding.getBoundContext().getProperty("value") as StockStatus;
+			if(sInStock === StockStatus.InStock) {
+				const cartModel = this.getView().getModel("cartModel") as JSONModel;
+				const aItems = cartModel.getProperty("/Products") as cartObject[] || [];
+				aItems.push({ price: fPrice, productId: sProductId, productTitle: sProductTitle, quantity: 1});
+				cartModel.setProperty("/Products", aItems);
+				MessageBox.information("Item added");
+			}
+		}).catch( ()=> {
+
+		});
+	}
+
+	/**
+	 * onPressCart
+	 */
+	public onPressCart() {
+		const oRouter = this.getRouter();
+		oRouter.navTo("cart");
 	}
 
 	/**
@@ -48,5 +97,10 @@ export default class Main extends BaseController {
 		const oListBinding = oGridList.getBinding("items") as ODataListBinding;
 		oListBinding.filter(oFilter);
 	}
+
+	/**
+	 * onPressProduct
+	 */
+	
 
 }
